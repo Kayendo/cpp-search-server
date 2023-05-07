0d5d854590e4f4737867f8538ced128337a6b0f3
Kayendo <vlad.sh.123@outlook.com>
2023-05-08 00:32:13 +0300

    six, fix

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -6,29 +6,25 @@
 
 using namespace std;
 
-void RemoveDuplicates(SearchServer& search_server) {
-    map<set<string>, int> word_freq;
-    set<int> ides;
-    for (const int id : search_server) {
-        map<string, double> word_frequencies = search_server.GetWordFrequencies(id);
-        set<string> words;
-        for (const auto& word : word_frequencies) {
-            words.insert(word.first);
-        }
-        if (word_freq.count(words)) {
-            int cur_id = word_freq.at(words);
-            int max_id = max(cur_id, id);
-            ides.insert(max_id);
-            int min_id = min(cur_id, id);
-            word_freq[words] = min_id;
-        }
-        else {
-           word_freq.insert(pair{ words, id });
-        }    
-    }
+void RemoveDuplicates(SearchServer &search_server) {
+	set<set<string>> word_freq;
+	set<int> ides;
+	for (const int id : search_server) {
+		set<string> words;
+		transform(search_server.GetWordFrequencies(id).begin(), search_server.GetWordFrequencies(id).end(), inserter(words, words.begin()), 
+                  [](const pair<string, double> &word_freqs) {
+					return word_freqs.first;
+				});
+        
+		if (word_freq.count(words)) {
+           ides.insert(id);
+		} else {
+			word_freq.insert(words);
+		}
+	}
 
-    for (const int id : ides) {
-        cout << "Found duplicate document id " << id << endl;
-        search_server.RemoveDocument(id);
-    }
+	for (int id : ides) {
+		search_server.RemoveDocument(id);
+		cout << "Found duplicate document id "s << id << endl;
+	}
 }
